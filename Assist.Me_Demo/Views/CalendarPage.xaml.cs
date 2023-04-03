8aab473a69a6cd17e9475c62f16d9f9efc9bc6d1
samuelpark <samuelkpark04@gmail.com>
2023-04-03 17:20:53 -0400

    commited views

---
+++ Assist.Me_Demo/Views/CalendarPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Maui;
+
+namespace Assist.Me_Demo.Views;
+
+public partial class CalendarPage : ContentPage
+{
+    public static CalendarPage Instance { get; private set; }
+    private static bool isChecked = false;
+	private static Border temp;
+	private static Button tempButton;
+
+    public CalendarPage()
+	{
+		InitializeComponent();
+		Instance = this;
+	}
+
+	public async static void moveCalendarUp()
+	{
+		Instance.EntireCalendar.TranslateTo(0, -143, 300, Easing.CubicIn);
+		await Task.Delay(500);
+		Instance.DisplaySchedule.FadeTo(1, 200, Easing.Linear);
+
+	}
+
+	public void CloseCalendar(System.Object sender, System.EventArgs e)
+	{
+		Navigation.PopAsync();
+	}
+
+	public async static void moveCalendarDown()
+	{
+        Instance.DisplaySchedule.FadeTo(0, 200, Easing.Linear);
+        await Task.Delay(300);
+        Instance.EntireCalendar.TranslateTo(0, 0, 300, Easing.CubicIn);
+    }
+
+	public static void Day1Clicked(System.Object sender, System.EventArgs e)
+	{
+        Button button = sender as Button;
+        Border border = (Border)button.Parent;
+
+        if (isChecked && border.BackgroundColor == Microsoft.Maui.Graphics.Colors.Transparent)
+		{
+			temp.BackgroundColor = Microsoft.Maui.Graphics.Colors.Transparent;
+			tempButton.TextColor = Microsoft.Maui.Graphics.Colors.White;
+            border.BackgroundColor = Microsoft.Maui.Graphics.Color.FromRgb(178, 164, 189);
+			button.TextColor = Microsoft.Maui.Graphics.Color.FromRgb(18, 17, 18);
+            Instance.ScheduleContent.Text = "Nothing scheduled for " + Instance.MonthName.Text + " " + button.Text;
+            temp = border;
+			tempButton = button;
+		}
+		else if (isChecked && border.BackgroundColor != Microsoft.Maui.Graphics.Colors.Transparent)
+		{
+            border.BackgroundColor = Microsoft.Maui.Graphics.Colors.Transparent;
+            button.TextColor = Microsoft.Maui.Graphics.Colors.White;
+			isChecked = false;
+			moveCalendarDown();
+        }
+		else
+		{
+			isChecked = true;
+			temp = border;
+			tempButton = button;
+			moveCalendarUp();
+			Instance.ScheduleContent.Text = "Nothing scheduled for " + Instance.MonthName.Text + " " + button.Text;
+            border.BackgroundColor = Microsoft.Maui.Graphics.Color.FromRgb(178, 164, 189);
+            button.TextColor = Microsoft.Maui.Graphics.Color.FromRgb(18, 17, 18);
+        }
+
+    }
+}
---
+++ Assist.Me_Demo/Views/StartPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿namespace Assist.Me_Demo.Views;
+
+public partial class StartPage : ContentPage
+{
+	private bool gettingUserPrompt = false;
+
+    public static StartPage Instance { get; private set; }
+    public StartPage()
+	{
+		InitializeComponent();
+		Instance = this;
+		//loadCalender();
+	}
+
+	public static void OpenCalendar(System.Object sender, System.EventArgs e)
+	{
+		loadCalender();
+	}
+	private static async void loadCalender()
+	{
+		await Instance.Navigation.PushAsync(new CalendarPage());
+	}
+
+	async void AssistClicked(System.Object sender, System.EventArgs e)
+	{
+
+		if (!gettingUserPrompt)
+		{
+            Button temp = (Button)sender;
+			await temp.FadeTo(0, 200);
+			await Task.Delay(200);
+			await FullPage.TranslateTo(0, -300, 300, Easing.CubicIn);
+			await Task.Delay(500);
+			await user_in.FadeTo(1, 200);
+			await SubmitButton.FadeTo(1, 200);
+			gettingUserPrompt = true;
+		}
+	}
+
+	async void SubmitClicked(System.Object sender, System.EventArgs e)
+	{
+		if (gettingUserPrompt)
+		{
+			
+            Button temp = (Button)sender;
+			await user_in.FadeTo(0, 200, Easing.CubicIn);
+			await temp.FadeTo(0, 200, Easing.CubicIn);
+			ChatBot.Text = "";
+			Intro.Text = ". . .";
+			await Task.Delay(400);
+			await FullPage.TranslateTo(0, 200,350, Easing.CubicIn);
+			await AssistButton.FadeTo(1, 200);
+			string temp_user_in = Instance.user_in.Text;
+			Instance.user_in.Text = "";
+			ProcessInput.process(temp_user_in);
+			gettingUserPrompt = false;
+		}
+    }
+
+
+	public static void updateChat(string up)
+	{
+		Instance.Intro.Text = "";
+		Instance.ChatBot.Text = up;
+	}
+
+}
